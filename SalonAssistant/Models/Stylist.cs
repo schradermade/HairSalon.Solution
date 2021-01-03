43b4c546ebf86896e76cb68b0b72154190111d8f
Nathan Schrader <nathanschrader@icloud.com>
2021-01-03 13:27:39 -0800

    add namespace, Stylist class, Stylists ctor, Find method

---
+++ SalonAssistant/Models/Stylist.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System;
+using Salon;
+
+namespace Salon.Models
+{
+  public class Stylist
+  {
+    public string Name { get; set; }
+    public DateTime HireDate { get; set; }
+    public int Id { get; set; }
+    private static List<Stylist> _stylists = new List<Stylist> { };
+    public List<Client> Clients { get; set; }
+
+    public Stylist(string name, DateTime hireDate)
+    {
+      Name = name;
+      HireDate = hireDate;
+      _stylists.Add(this);
+      Id = _stylists.Count;
+      Clients = new List<Client> { };
+    }
+
+    public static Stylist Find (int findId)
+    {
+      
+    }
+  }
+}
