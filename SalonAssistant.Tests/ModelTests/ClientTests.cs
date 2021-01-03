16afc64b7b5aaa6887740d1d9fd0a90ca9dd2bb8
Nathan Schrader <nathanschrader@icloud.com>
2021-01-03 14:59:13 -0800

    add Good Fail : ClientConstructor test

---
+++ SalonAssistant.Tests/ModelTests/ClientTests.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Salon.Models;
+using System;
+
+namespace Salon.Test
+{
+  [TestClass]
+  public class ClientTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Client.ClearAll();
+    }
+
+    [TestMethod]
+    public void ClientConstructor_CreatesInstanceOfClient_Client()
+    {
+      // Arrange
+      int stylistId = 1;
+      string name = "Crystal";
+      string styleNotes = "straight hair";
+      double stylePrice = 75;
+      string dateJoin = "12/31/2017";
+      Client newClient = new Client(stylistId, name, styleNotes, stylePrice, dateJoin);
+
+      // Act
+      Assert.AreEqual(typeof(Stylist), newClient.GetType());
+    }
+  }
+}
